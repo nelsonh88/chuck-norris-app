8a81b6be132fcd78f2bf5554aac79fb49772017a
nelsonh88 <nelson.huezo88@gmail.com>
2022-01-20 11:19:12 -0500

    Updates the API     - updated the API call to a new API

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 function App() {
   async function fetchBurn() {
-    const response = await fetch(
-      "https://cors-anywhere.herokuapp.com/https://yomomma-api.herokuapp.com/jokes",
-      {
-        method: "GET",
-      }
-    );
+    const response = await fetch("https://api.chucknorris.io/jokes/random", {
+      method: "GET",
+    });
     const data = await response.json();
-    console.log(data);
+    console.log(data.value);
   }
   return <button onClick={fetchBurn}>Click me</button>;
 }
