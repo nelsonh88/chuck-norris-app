3398055a22a471a86f884e3c27e64c20f6de5478
nelsonh88 <nelson.huezo88@gmail.com>
2022-01-19 19:59:36 -0500

    Makes a request to the yomomma api     - I have made a succesful request to the api     - I ended up sending my request through a proxy since I kept on getting the error of 'No "Access-Control-Allow-Origin"' and the header of Access-Control-Allow-Origin: * did not work. This is due to my localhost server

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  async function fetchBurn() {
+    const response = await fetch(
+      "https://cors-anywhere.herokuapp.com/https://yomomma-api.herokuapp.com/jokes",
+      {
+        method: "GET",
+      }
+    );
+    const data = await response.json();
+    console.log(data);
+  }
+  return <button onClick={fetchBurn}>Click me</button>;
 }
 
 export default App;
