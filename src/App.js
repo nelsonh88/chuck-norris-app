ef264c26dcd243b688f4fc9668d8732369cd4cbe
nelsonh88 <nelson.huezo88@gmail.com>
2022-01-22 12:37:00 -0500

    Updates UI     - created a wrapper component     - add css modules     - updated the incoming data from the API and formatted to meet my apps needs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
+import React, { useState } from "react";
+
+import Wrapper from "./components/Layout/Wrapper";
+import Fact from "./components/Fact";
+
 function App() {
+  const [fact, setFact] = useState(null);
+
   async function fetchBurn() {
     const response = await fetch("https://api.chucknorris.io/jokes/random", {
       method: "GET",
     });
     const data = await response.json();
-    console.log(data.value);
+    console.log(data);
+
+    const factData = {
+      id: data.id,
+      image: data.icon_url,
+      fact: data.value,
+      category: data.categories,
+    };
+    console.log(factData);
+    setFact(factData);
   }
-  return <button onClick={fetchBurn}>Click me</button>;
+  return (
+    <React.Fragment>
+      <main>
+        <Wrapper>
+          {fact ? <Fact id={fact.id} fact={fact.fact}></Fact> : ""}
+          <button onClick={fetchBurn}>Click me</button>
+        </Wrapper>
+      </main>
+    </React.Fragment>
+  );
 }
 
 export default App;
